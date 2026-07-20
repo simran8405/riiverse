94206d5f7ac33a6a4255320eeefc06e21f989da6
simran8405 <simranjeet0804kaur@gmail.com>
2026-07-20 22:19:02 +0530

    Create homepage and organize React project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,8 @@
 import "./App.css";
+import Home from "./pages/Home";
+
 function App() {
-  return (
-    <div>
-      <h1>Welcome to Riiverse 🛒</h1>
-      <p>My Full Stack MERN E-Commerce Website</p>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ client/src/pages/home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Riiverse 🛒</h1>
+      <p>Your one-stop shopping destination.</p>
+    </div>
+  );
+}
+
+export default Home;
