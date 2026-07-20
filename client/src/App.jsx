bf7361c4512b6dd3930fb555591cdbba8cd1dc69
simran8405 <simranjeet0804kaur@gmail.com>
2026-07-20 21:06:12 +0530

    Create initial Riiverse homepage

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Riiverse 🛒</h1>
+      <p>My Full Stack MERN E-Commerce Website</p>
+    </div>
+  );
+}
+
+export default App;
